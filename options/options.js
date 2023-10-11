df3cea6ae573ed27133f2b512dac1d3456f72148
Crowdbotics <engineering@crowdbotics.com>
2023-10-11 07:00:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-salad-dev-121670",
+  url: "https://dark-salad-dev-121670.botics.co",
+  api: "https://dark-salad-dev-121670.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
